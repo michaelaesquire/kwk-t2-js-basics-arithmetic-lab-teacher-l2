4d636752dbde5d00159adff9a05d2a7b029ee3f3
Michaela Olson <mco44@cornell.edu>
2018-05-08 21:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,5 @@ let ageIsValid = Number.isInteger(currentAge);
 let randomNumber= Math.floor(Math.random() * 20) + 1;
 
 let randomInteger = Math.floor(randomNumber);
+
+let randomUserID = randomInteger + 1000000000;
